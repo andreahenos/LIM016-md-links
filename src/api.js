c2a870bccef7011ed27c08a2fd87d12ec92bd97e
andreahenos <henostroza.andrea@gmail.com>
2022-01-20 11:20:45 -0500

    Creando función única para obtener array de files

--- src/api.js
+++ src/api.js
@@ -4,48 +4,24 @@ import path from 'path';
 // comprobar validez
 export const verifyPathExistence = (route) => fs.existsSync(route);
 
-/*
-// comprobar validez
-export const verifyPathExistence = (route) => {
-  const promise = new Promise((resolve, reject) => {
-    fs.access(route, fs.F_OK, (err) => {
-      if (err) return reject('1. Path does not exist.');
-      // console.log('1. Path does not exist.');
-      return resolve('1. Path exist.');
-    });
-  });
-  return promise;
-}; */
-
-/*
-// comprobar validez
-export const verifyPathExistence = (route) => {
-  fs.access(route, fs.F_OK, (err) => {
-    if (err) return false;
-    // console.log('1. Path does not exist.');
-    return console.log('1. Path exist');
-  });
-}; */
-
 // ver tipo de path y convertir
-export const getAbsolutePath = (route) => (!path.isAbsolute(route)
-  ? console.log('2. File: ', path.resolve(route))
-  : console.log('2. File: ', route));
+const getAbsolutePath = (route) => (!path.isAbsolute(route)
+  ? path.resolve(route)
+  : route);
 
 // comprobar tipo de archivo
-export const verifyFileType = (route) => {
+const verifyFileType = (route) => {
   const promise = new Promise((resolve, reject) => {
     fs.lstat(route, (err, stats) => {
-      if (err) reject('3. An error has occurred: ', err);
-      const verify = !stats.isFile() ? '3. Stat: Directory' : '3. Stat: File';
-      resolve(verify);
+      if (err) reject(err);
+      resolve(stats.isFile());
     });
   });
   return promise;
 };
 
 // leer directorio
-export const readDirectory = (route) => {
+const readDirectory = (route) => {
   const promise = new Promise((resolve, reject) => {
     fs.readdir(route, (err, files) => {
       if (err) reject(err);
@@ -56,12 +32,38 @@ export const readDirectory = (route) => {
 };
 
 // comprobar extensión
-export const getExtension = (route) => path.extname(route);
+const getExtension = (route) => path.extname(route);
 
 // leer archivos
 export const readFiles = (route) => {
-  fs.readFile(route, 'utf8', (err, content) => {
-    if (err) return console.log(err);
-    return console.log(content);
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(route, 'utf8', (err, content) => {
+      if (err) reject(err);
+      resolve(content);
+    });
   });
+  return promise;
+};
+
+// obtener archivos md
+export const getMdFiles = (route) => {
+  const absolutePath = getAbsolutePath(route);
+
+  verifyFileType(absolutePath)
+    .then((res) => {
+      if (res) {
+        if (getExtension(absolutePath) === '.md') return console.log(absolutePath.split(' '));
+        return console.log('It is not an md format file');
+      }
+      readDirectory(absolutePath)
+        .then((files) => {
+          const searchMdFiles = files.filter((file) => getExtension(file) === '.md');
+          if (files === [] || searchMdFiles === []) return ('There are no files with md format.');
+          const convertAbsolute = searchMdFiles.map((file) => getAbsolutePath(file));
+          return console.log(convertAbsolute);
+        })
+        .catch((err) => console.log('An error has occurred: ', err));
+      return 'Obtained md file paths';
+    })
+    .catch((err) => console.log('An error has occurred: ', err));
 };
--- src/md-links.js
+++ src/md-links.js
@@ -1,40 +1,17 @@
 import {
   verifyPathExistence,
-  getAbsolutePath,
-  verifyFileType,
-  readDirectory,
-  getExtension,
-  readFiles,
+  getMdFiles,
 } from './api.js';
 
 function mdLinks(path) {
   try {
-    if (verifyPathExistence(path)) {
-      console.log('1. Path exist.');
-      getAbsolutePath(path);
-      verifyFileType(path)
-        .then((verifyMessage) => {
-          console.log(verifyMessage);
-          if (verifyMessage === '3. Stat: Directory') {
-            readDirectory(path)
-              .then((files) => {
-                const getMdFiles = files.filter((file) => getExtension(file) === '.md');
-                if (getMdFiles === []) console.log('There are no md files.');
-                console.log(getMdFiles);
-                readFiles('C:\\Users\\51960\\Desktop\\Md-Links\\LIM016-md-links\\Modelo\\prueba.js');
-              })
-              .catch((err) => console.log(err));
-          }
-        })
-        .catch((err) => console.log(err));
-    } else {
-      console.log('1. Path does not exist.');
-    }
+    if (verifyPathExistence(path)) return getMdFiles(path);
+    return console.log('Path does not exist.');
   } catch (err) {
-    console.log('1. An error has occurred: ', err);
+    return console.log('An error has occurred: ', err);
   }
 }
 
 mdLinks('./Modelo');
 // mdLinks('./Modelo/prueba2.md');
-// mdLinks('C:\\Users\\51960\\Desktop\\Md-Links\\LIM016-md-links\\Modelo\\prueba.js');
+// mdLinks('C:\\Users\\51960\\Desktop\\Md-Links\\LIM016-md-links\\Modelo');
