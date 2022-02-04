b98b6f13d79a107790d485237d75ae838489c043
andreahenos <henostroza.andrea@gmail.com>
2022-02-04 10:16:37 -0500

    Refactorización de CLI

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
 import process from 'process';
+import readline from 'readline';
+import { verifyPathExistence } from './api.js';
 import { mdLinks } from './md-links.js';
 import {
   totalLinks,
@@ -6,7 +8,120 @@ import {
   brokenLinks,
 } from './stats.js';
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const args = process.argv;
+
+// Option messages
+
+const helpOption = () => {
+  process.stdout.write(`\n> USAGE:
+  mdLinks [-help] or [-h] ------------------------------- To see all options
+  mdLinks <path> ---------------------------------------- To have href, text and file
+  mdLinks <path> [-validate] or [-v] -------------------- To have href, text, file, status and "ok" message
+  mdLinks <path> [-stats] or [-s] ----------------------- To have total and unique links
+  mdLinks <path> [-validate] [-stats] or [-v] [-s] ------ To have total, unique and broken links\n`);
+  rl.close();
+  process.exit(0);
+};
+
+const defaultOption = (arg) => {
+  mdLinks(arg, { validate: false })
+    .then((res) => {
+      process.stdout.write(res.map((one) => `\nHref: ${one.href}\nText: ${one.text}\nFile: ${one.file}\n`).join(' '));
+      rl.close();
+      process.exit(0);
+    })
+    .catch((err) => {
+      process.stdout.write(err);
+      rl.close();
+      process.exit(0);
+    });
+};
+
+const verifyPathAndDefaultOption = () => {
+  if (verifyPathExistence(args[2]) === false) {
+    process.stdout.write("\n>  PATH DOESN'T EXIST.\n");
+    rl.close();
+    process.exit(0);
+  } else {
+    defaultOption(args[2]);
+  }
+};
+
+const validatetOption = (arg) => {
+  mdLinks(arg, { validate: true })
+    .then((res) => {
+      process.stdout.write(res.map((one) => `\nHref: ${one.href}\nText: ${one.text}\nFile: ${one.file}\nStatus: ${one.status}\nOk: ${one.ok}\n`).join(' '));
+      rl.close();
+      process.exit(0);
+    })
+    .catch((err) => {
+      process.stdout.write(err);
+      rl.close();
+      process.exit(0);
+    });
+};
+
+const statsOption = (arg) => {
+  mdLinks(arg, { stats: true })
+    .then((res) => {
+      process.stdout.write(`\nTotal: ${totalLinks(res)}\nUnique: ${uniqueLinks(res)}\n`);
+      rl.close();
+      process.exit(0);
+    })
+    .catch((err) => {
+      process.stdout.write(err);
+      rl.close();
+      process.exit(0);
+    });
+};
+
+const validateAndStatsOption = (arg) => {
+  mdLinks(arg, { stats: true })
+    .then((res) => {
+      process.stdout.write(`\nTotal: ${totalLinks(res)}\nUnique: ${uniqueLinks(res)}\nBroken: ${brokenLinks(res)}\n`);
+      rl.close();
+      process.exit(0);
+    })
+    .catch((err) => {
+      process.stdout.write(err);
+      rl.close();
+      process.exit(0);
+    });
+};
+
+// answers
+
+switch (args.length) {
+  case 3:
+    if (args[2] === '-help' || args[2] === '-h') {
+      helpOption();
+    } else {
+      verifyPathAndDefaultOption();
+    }
+    break;
+  case 4:
+    if (args[3] === '-validate' || args[3] === '-v') {
+      validatetOption(args[2]);
+    } else if (args[3] === '-stats' || args[3] === '-s') {
+      statsOption(args[2]);
+    }
+    break;
+  case 5:
+    if ((args[3] === '-validate' && args[4] === '-stats') || (args[3] === '-v' && args[4] === '-s')) {
+      validateAndStatsOption(args[2]);
+    }
+    break;
+  default:
+    process.stdout.write(helpOption());
+    break;
+}
+
+/* const args = process.argv;
 
 if (args.length === 2) {
   console.log('Please enter a path.');
@@ -43,7 +158,7 @@ if (args.length === 4 && (args[3] === '-validate' || args[3] === '-v')) {
 }
 
 if (args.length === 4 && (args[3] === '-stats' || args[3] === '-s')) {
-  mdLinks(args[2], { validate: true })
+  mdLinks(args[2], { stats: true })
     .then((res) => {
       if (typeof res !== 'string') {
         console.log(`\nTotal: ${totalLinks(res)}\nUnique: ${uniqueLinks(res)}\n`);
@@ -64,4 +179,4 @@ if (args.length === 5 && ((args[3] === '-validate' && args[4] === '-stats') || (
       }
     })
     .catch((err) => console.log(err));
-}
+} */
--- src/md-links.js
+++ src/md-links.js
@@ -1,24 +1,16 @@
 import {
-  verifyPathExistence,
   getProperties,
 } from './api.js';
 
 export function mdLinks(path, option) {
   const promise = new Promise((resolve, reject) => {
     try {
-      if (verifyPathExistence(path)) {
-        Promise.all(getProperties(path))
-          .then((res) => resolve(res.flat()));
-      } else if (option.help) {
-        resolve(`   Usage:
-        mdLinks <path> ------------------------------------------ To have href, text and file
-        mdLinks <path> [-validate] or [-v] -------------------- To have href, text, file, status and "ok" message
-        mdLinks <path> [-stats] or [-s] ----------------------- To have total and unique links
-        mdLinks <path> [-validate] [-stats] or [-v] [-s] ---- To have total, unique and broken links
-        mdLinks <path> [-help] or [-h] ------------------------ To see all options`);
-      } else resolve("Path does'nt exist.");
+      Promise.all(getProperties(path))
+        .then((res) => {
+          resolve(res.flat());
+        });
     } catch (err) {
-      reject(`An error has occurred: ${err.message}`);
+      reject(`\n> AN ERROR HAS OCURRED:  ${err.message}\n`);
     }
   });
   return promise;
